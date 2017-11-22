d8f3ae1181f17b05e701156177a133f17c977da2
Colin Adema <cadema@ufl.edu>
2017-11-22 13:10:18 -0500

    Thought about the methods we hae to code

--- System/game/controllers/DefenderController.java
+++ System/game/controllers/DefenderController.java
@@ -12,5 +12,7 @@ public interface DefenderController
 {
 	public void init(Game game);
 	public int[] update(Game game, long timeDue);
+	//Takes the position of the enemy and uses that to determine where to go next?
+	//Probably wouldn't be efficient enough to just chase after the attacker, so maybe somehow we can program the defenders to corner it?
 	public void shutdown(Game game);
 }
