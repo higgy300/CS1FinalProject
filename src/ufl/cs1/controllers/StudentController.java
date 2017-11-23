354a96b63d060aef2ca67d4dfe30b1be36b6f87f
higgy300 <jhigueraballesteros@gmail.com>
2017-11-23 14:39:10 -0500

    11/23/17 2:37 PM - Started working on the AI. Almost done writing the chasing state for the gator

--- src/ufl/cs1/controllers/StudentController.java
+++ src/ufl/cs1/controllers/StudentController.java
@@ -8,10 +8,17 @@ import java.util.List;
 
 public final class StudentController implements DefenderController
 {
+	Game newState, currentState;
+
+	// This is how many gators are supposed to exist in the game
+	private final int Gator1 = 0;
+	private final int Gator2 = 1;
+	private final int Gator3 = 2;
+	private final int Gator4 = 3;
+
+	// init(), shutdown(), update() are mandatory from the DefenderController interface
 	public void init(Game game) { }
-
 	public void shutdown(Game game) { }
-
 	public int[] update(Game game,long timeDue)
 	{
 		int[] actions = new int[Game.NUM_DEFENDER];
@@ -30,4 +37,78 @@ public final class StudentController implements DefenderController
 		}
 		return actions;
 	}
+
+
+	// Created this interface to check current and future state
+	private interface CheckState {
+		void updateTimer(long newTime);
+
+		StudentController.stateType getCurrentState();
+		StudentController.stateType getFutureState(int nxtState);
+		int getNextDirection(int dir);
+		void reset();
+
+	}
+
+	// This state defines when the gator is chashing the attacker
+	private class KillState implements StudentController.CheckState {
+		private int switchOutOfExplore;
+		private long timer, futureTime, previousTime;
+
+		// default constructor initializes everything to ZERO
+		private KillState() {
+			this.switchOutOfExplore = 0;
+			this.timer = 0L;
+			this.futureTime = 0L;
+			this.previousTime = 0L;
+		}
+
+		// This will reset all attributes of this class
+		public void reset() {
+			this.switchOutOfExplore = 0;
+			this.timer = 0L;
+			this.futureTime = 0L;
+			this.previousTime = 0L;
+		}
+		public StudentController.stateType getCurrentState() {
+			return stateType.Kill;
+		}
+		public StudentController.stateType getFutureState(int whichGator) {
+			if (this.switchOutOfExplore < 3 && this.timer >= 20000L) {
+				++this.switchOutOfExplore;
+				return stateType.explore;
+			}
+			// This will check if gator can be killed. If true, will change state to run away.
+			else if(StudentController.this.currentState.getDefender(whichGator).isVulnerable()) {
+				return stateType.runAway;
+			}
+			// If nothing changes:
+			else {
+				return stateType.unchanged;
+			}
+		}
+
+		@Override
+		public int getNextDirection(int dir) {
+			// FIX ME
+			return 0;
+		}
+
+		public void updateTimer(long timeNow) {
+			this.futureTime = 40L;
+			this.timer += this.futureTime;
+			this.previousTime = timeNow;
+		}
+
+		// This method will
+		public int getMovingPossibilities(int whichGator) {
+			// FIX ME
+			return 0;
+		}
+	}
+
+	static enum stateType { Kill, runAway, explore, Jailed, unchanged;
+		private stateType(){}}
+
+
 }
