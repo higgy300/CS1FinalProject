007bc3dcc058ae0d956515004d130f7e72b11890
Colin Adema <cadema@ufl.edu>
2017-11-28 19:36:11 -0500

    fixed the StudentController.java default constructor and branched off from master to test out the new change in the constructor.

--- System/game/controllers/DefenderController.java
+++ System/game/controllers/DefenderController.java
@@ -12,7 +12,5 @@ public interface DefenderController
 {
 	public void init(Game game);
 	public int[] update(Game game, long timeDue);
-	//Takes the position of the enemy and uses that to determine where to go next?
-	//Probably wouldn't be efficient enough to just chase after the attacker, so maybe somehow we can program the defenders to corner it?
 	public void shutdown(Game game);
 }
